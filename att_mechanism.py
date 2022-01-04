adbea0809675e4b7e58a4b5b607118ff32a586fc
HalimSD <maingateway1@gmail.com>
2022-01-04 07:20:21 +0100

    adding the encoder

--- att_mechanism.py
+++ att_mechanism.py
@@ -67,3 +67,32 @@ class TransformerBlock(nn.Module):
         out = self.dorpout(self.norm2(forward+x))
         return out
 
+class Encoder(nn.Module):
+    def __init__(self, src_vocab_size, embed_size, num_layers, heads, 
+                    forward_expansion, dropout, max_lenght, device):
+        super(Encoder, self).__init__()
+
+        self.embed_size = embed_size
+        self.device = device
+
+        self.word_embedding = nn.Embedding(src_vocab_size, embed_size)
+        self.position_embedding = nn.Embedding(max_lenght, embed_size)
+
+        self.layers = nn.ModuleList([
+            TransformerBlock(embed_size, heads, dropout,forward_expansion)
+        ])
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask):
+        N, seq_len = x.shape
+        positions = torch.arange(0, seq_len).expand(N, seq_len).to(self.device)
+
+        out = self.dropout(self.word_embedding(x) + self.position_embedding(positions))
+
+        for layer in self.layers:
+            out = layer(out,out,out,mask)
+
+        return out
+        
+
